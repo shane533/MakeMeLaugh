6ddd85c70122bc0c15e4e3cf7ca8a5bc6706ac05
delategirl <952436231@qq.com>
2024-01-27 20:45:31 +0800

    update Cutscenes

---
+++ Background.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+var path = "res://insulting_scene.tscn"
+
+func _ready():
+	var label_num = $Label.get_total_character_count()
+	var i = 0
+	$Label.show()
+	while i <= label_num:
+		$Label.visible_characters = i
+		i += 1
+		var time1 = get_tree().create_timer(0.2)
+		await time1.timeout
+	var time2 = get_tree().create_timer(3)
+	await time2.timeout
+	get_tree().change_scene_to_file(path)
--- scene_changer.gd
+++ scene_changer.gd
@@ -5,11 +5,10 @@ extends Node2D
 @export var v_window = 2000
 @export var v_building = 5000
 
-var path = "res://insulting_scene.tscn"
+var path = "res://Background.tscn"
 
 func _ready():
-	self._label_emergence()
-	#get_tree().change_scene_to_file(path)
+	pass
 
 func _process(delta):
 	var pos = $Background1.position.move_toward(Vector2.ZERO,delta*v_background)
@@ -22,16 +21,5 @@ func _process(delta):
 	$Building2.position = pos
 	pos = $Building3.position.move_toward(Vector2.ZERO,delta*v_building)
 	$Building3.position = pos
-
-func _label_emergence():
-	var label_num = $Label.get_total_character_count()
-	var i = 0
-	$Label.show()
-	while i <= label_num:
-		$Label.visible_characters = i
-		i += 1
-		var time1 = get_tree().create_timer(0.2)
-		await time1.timeout
-	var time2 = get_tree().create_timer(3)
-	await time2.timeout
-
+	if $Window.position.y < 60:
+		get_tree().change_scene_to_file(path)
