55b2b28978818e2b0b06ffa13c09e1615cece3fe
shane533 <ptez533@gmail.com>
2024-01-27 23:49:35 +0800

    update

--- global.gd
+++ global.gd
@@ -4,6 +4,11 @@ class_name GlobalNode
 
 @export var is_ZH:bool = false 
 @export var is_2020:bool = true
+
+var story_1_ZH: String = "测试测试测试1"
+var story_1_EN: String = "TESTTEST1"
+var story_2_ZH: String = "测试测试测试1"
+var story_2_EN: String = "TESTTEST2"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
