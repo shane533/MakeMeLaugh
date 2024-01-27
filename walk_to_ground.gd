67c090dc967fa02e92362be997be3921ce483c49
delategirl <952436231@qq.com>
2024-01-28 00:07:22 +0800

    update

--- walk_to_ground.gd
+++ walk_to_ground.gd
@@ -1,22 +1,25 @@
 extends Node2D
 
 @export var vec_player = Vector2(175,520)
-@export var v_player = 1000
+@export var v_player = 200
 @export var vec_enemy = Vector2(1080,520)
-@export var v_enemy = 1000
-@onready var animation_player = $AnimationPlayer
-@onready var animation_enemy = $AnimationEnemy
+@export var v_enemy = 200
+@onready var player_sprite2D = $PlayerSprite2D
+@onready var enemy_sprite2D = $EnemySprite2D
 
 func _ready():
 	pass
 
 func _process(delta):
-	var pos = animation_player.position.move_toward(vec_player,delta*v_player)
-	animation_player.position = pos
-	pos = animation_enemy.position.move_toward(vec_enemy,delta*v_enemy)
-	animation_enemy.position = pos
-	if animation_player.position.x >= 175:
-		animation_player.play("idle")
+	var pos = player_sprite2D.position.move_toward(vec_player,delta*v_player)
+	player_sprite2D.position = pos
+	pos = enemy_sprite2D.position.move_toward(vec_enemy,delta*v_enemy)
+	enemy_sprite2D.position = pos
+	if player_sprite2D.position.x >= 175:
+		player_sprite2D.play("idle")
 	else:
-		animation_player.play("walk")
-		
+		player_sprite2D.play("walk")
+	if enemy_sprite2D.position.x <= 1080:
+		enemy_sprite2D.play("idle")
+	else:
+		enemy_sprite2D.play("walk")
