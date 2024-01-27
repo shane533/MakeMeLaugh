e1d5c6ea013f1cbd4185d43c08e55e89bab06c83
delategirl <952436231@qq.com>
2024-01-27 16:07:33 +0800

    change Cutscenes from video to text emergence

--- scene_changer.gd
+++ scene_changer.gd
@@ -3,9 +3,15 @@ extends Node2D
 var path = "res://insulting_scene.tscn"
 
 func _ready():
-	$VideoStreamPlayer.show()
-	$VideoStreamPlayer.play()
+	var label_num = $Label.get_total_character_count()
+	var i = 0
 	
+	while i <= label_num:
+		$Label.visible_characters = i
+		i = i + 1
+		var time1 = get_tree().create_timer(0.2)
+		await time1.timeout
+	var time2 = get_tree().create_timer(3)
+	await time2.timeout
 	
-func _on_video_stream_player_finished():
 	get_tree().change_scene_to_file(path)
--- start_scene.gd
+++ start_scene.gd
@@ -4,7 +4,7 @@ var path = "res://scene_changer.tscn"
 	
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file(path)
-		
+	
 
 func _on_exit_button_pressed():
 	get_tree().quit()
